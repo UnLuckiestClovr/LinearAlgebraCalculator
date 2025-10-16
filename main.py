3ea60730ab5f79cb3f7b46d5ebefe391389e5595
UnLuckiestClovr <urluckyclovr@gmail.com>
2025-10-15 18:53:13 -0600

    Fully moving to an API Based Setup for now.

--- main.py
+++ main.py
@@ -15,66 +15,17 @@ def measure_response_time(start_time):
     print("Response time:", time.perf_counter() - start_time)
 
 
-def perform_operation(op: dict):
-    start_time = time.perf_counter()
-    try:
-        match op['opname']:
-            case 'add':
-                result = matrix_math.iterative_addition(op['matrices'])
+# """
+# JSON Structure:
 
-                print(result)
-            case 'solve':
-                m_rowoperations.solve_matrix_values(np.array(op['matrices']))
-            case _:
-                return None # None is Python's version of 'null'
-    except Exception as e:
-        print("Error: ", e)
-        # Log the exception along with additional information
-        logging.info('An error occurred: %s', str(e))
-    measure_response_time(start_time)
-
-
-
-def handle_equations():
-    try:
-        with open('input.json', 'r') as f:
-            data = json.load(f)
-
-            print(f'Data Loaded: {data} | {type(data)}')
-
-            npArray = np.array(data)
-
-            for operation in npArray:
-                perform_operation(operation)
-
-    except FileNotFoundError:
-        print("input.json file not found. Please create the file and add your equations and arrays.")
-    except Exception as e:
-        print("Error: ", e)
-        # Log the exception along with additional information
-        logging.info('An error occurred: %s', str(e))
-
-while(True):
-    print("Enter Equations and Arrays into JSON file. Code will handle the rest.")
-    user_input = input("Please enter your equations and arrays then press Enter (or type 'exit' to quit): ")
-
-    if user_input.lower() == 'exit':
-        break
-    else:
-        handle_equations()
-
-
-"""
-JSON Structure:
-
-{
-    "opname": <string>,
-    "matrices": [
-        [[1, 2], [3, 4]],
-        [[5, 6], [7, 8]]
-    ]
-}
-"""
+# {
+#     "opname": <string>,
+#     "matrices": [
+#         [[1, 2], [3, 4]],
+#         [[5, 6], [7, 8]]
+#     ]
+# }
+# """
 
 if __name__ == "__main__":
     import uvicorn
@@ -84,7 +35,7 @@ if __name__ == "__main__":
         version="1.0.0"
     )
 
-    app.include_router(router=main_routes.router, prefix="/api")
+    app.include_router(router=main_routes.router)
 
     @app.get("/")
     def read_root():
